8402ba26017c2750cd21efd020d6b140f4e3b16f
May <mayyuhein@gmail.com>
2021-05-10 05:23:33 -0400

    Added Swiper.js and styled Nominateions Section

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../App.css'
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ['btn--primary', 'btn--outline', 'btn--remove']
 
-const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide']
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', ]
 
 const COLORS = ['primary', 'blue', 'red', 'green', 'dark-blue', 'pink']
 
--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -3,24 +3,44 @@ import { connect } from 'react-redux';
 import '../App.css';
 import { removeNominationThunk } from '../redux/app-redux';
 import { Button } from './Button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import 'swiper/swiper-bundle.css'
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function NominatedMovies({ nominatedMovies, removeNomination }) {
   return (
-    <>
-      <section class="nominations">
-        <h2 class="sectionTitle">Nominated Movies</h2>
-          <a href="#section3">‹</a>
+    <div className="darkBackground">
+      <h2 className="sectionTitle">Nominated Movies</h2>
+      <section className="nominations">
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={5}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            loop={true}
+            speed={500}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
           {nominatedMovies.map((movie) => {
             return (
-              <div class="item" key={movie.imdbID}>
-                <p>{movie.Title} ({movie.Year})</p>
-                <button onClick={() => removeNomination(movie.imdbID)}>Remove Nomination</button>
-              </div>
+              <SwiperSlide key={movie.imdbID}>
+                <div className="swiper-slide">
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <p>{movie.Title} ({movie.Year})</p>
+                  <Button buttonStyle="btn--remove" onClick={() => removeNomination(movie.imdbID)}>Remove Nomination</Button>
+                </div>
+              </SwiperSlide>
             )
           })}
-          <a href="#section3">›</a>
+          </Swiper>
       </section>
-    </>
+    </div>
   );
 }
 
